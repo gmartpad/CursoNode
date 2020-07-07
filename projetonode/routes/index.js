7488f86309644ead55102c085376a930628ec1ad
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-07-07 20:49:18 -0300

    Configurando Template Engine

--- projetonode/routes/index.js
+++ projetonode/routes/index.js
@@ -1,30 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
-router.get('/', (req, res) => {
-    res.json(req.query);
-
-    // GET: req.query
-    // POST: req.body
-    // PARAMETROS DA URL: req.params
-
-    // SEND
-    // JSON
-
-});
-
-router.get('/posts/:slug', (req, res)=>{
-    let slug = req.params.slug;
-
-    // Titulo: seja bem vindo
-    // Slug: seja-bem-vindo
-
-    res.send('Slug do post: '+slug);
-});
-
-
-router.get('/sobre', (req, res)=>{
-    res.send('Página SOBRE');
+router.get('/', (req, res) => {    
+    let obj = {
+        'nome': 'Gabriel',
+        'idade': 19
+    }
+    res.render('home', obj);
 });
 
 module.exports = router;
