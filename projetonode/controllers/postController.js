da532139b14b46fa5ca16ade99d2622baf075e2d
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-07-14 20:38:31 -0300

    Editar Post

--- projetonode/controllers/postController.js
+++ projetonode/controllers/postController.js
@@ -20,3 +20,28 @@ exports.addAction = async (req, res) => {
 
     res.redirect('/');
 };
+
+exports.edit = async (req, res) => {
+    // 1. Pegar as informações do post em questão.
+    const post = await Post.findOne({ slug:req.params.slug });
+    // 2. Carregar o formulário de edição.
+    res.render('postEdit', { post });
+};
+
+exports.editAction = async (req, res) => {
+    // Procutar o item enviado.
+    // Pegar os dados e atualizar.
+    const post = await Post.findOneAndUpdate(
+        { slug:req.params.slug }, 
+        req.body, 
+        { 
+            new:true, // Retornar NOVO item atualizado
+            runValidators: true
+        }
+    );
+    // Mostrar mensagem de sucesso e redirecionar para a home.
+
+    req.flash('success', 'Post atualizado com sucesso!');
+    
+    res.redirect('/');
+};
--- projetonode/routes/index.js
+++ projetonode/routes/index.js
@@ -8,5 +8,7 @@ router.get('/', homeController.index);
 router.get('/users/login', userController.login);
 router.get('/post/add', postController.add);
 router.post('/post/add', postController.addAction);
+router.get('/post/:slug/edit', postController.edit);
+router.post('/post/:slug/edit', postController.editAction);
 
 module.exports = router;
