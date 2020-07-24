8232b2896e3ccad43a5cbbca2366291d81daac80
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-07-24 18:23:37 -0300

    Alterar a senha

--- projetonode/middlewares/authMiddleware.js
+++ projetonode/middlewares/authMiddleware.js
@@ -1,4 +1,4 @@
-module.exports.isLogged = (req, res, next) => {
+exports.isLogged = (req, res, next) => {
     if(!req.user) {
         req.flash('error', 'Ops! Você não tem permissão para acessar esta página.');
         res.redirect('/users/login');
@@ -7,3 +7,21 @@ module.exports.isLogged = (req, res, next) => {
 
     next();
 };
+
+exports.changePassword = (req, res) => {
+    // 1. Confirmar que as senhas batem.
+    if(req.body.password != req.body['password-confirm']){
+        req.flash('error', 'Senhas não batem');
+        res.redirect('/profile');
+        return;
+    }
+    // 2. Procurar o usuário e trocar a senha dele.
+    req.user.setPassword(req.body.password, async () => {
+        await req.user.save();
+
+        // 3. Redirecionar para a HOME
+        req.flash('success', 'Senha alterada com sucesso');
+        res.redirect('/');
+   
+    });
+}
--- projetonode/routes/index.js
+++ projetonode/routes/index.js
@@ -18,6 +18,8 @@ router.post('/users/register', userController.registerAction);
 router.get('/profile', authMiddleware.isLogged, userController.profile);
 router.post('/profile', authMiddleware.isLogged, userController.profileAction);
 
+router.post('/profile/password', authMiddleware.isLogged, authMiddleware.changePassword)
+
 router.get('/post/add', authMiddleware.isLogged, postController.add);
 router.post('/post/add', 
     authMiddleware.isLogged,
