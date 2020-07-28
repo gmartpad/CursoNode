811b67b7c5f4496dac9d7e281b7d0e6d347ddb78
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-07-28 16:53:00 -0300

    Estabelecendo conexão

---
+++ chatnode/public/main.js
@@ -0,0 +1,2 @@
+//listener no client
+const socket = io();
--- chatnode/server.js
+++ chatnode/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+
+//criando o socket relacionado ao servidor
+const io = socketIO(server);
 
 //determinar porta
 server.listen(3000);
 
 //ler a pasta public
 app.use(express.static(path.join(__dirname, 'public')));
+
+//listener
+io.on('connection', (socket) => {
+    console.log("Conexão detectada...");
+})
