81adb2cb386259f2f8db54b28b46f9f7b3f14d43
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-07-28 23:41:44 -0300

    Entrando no chat (2/2)

--- chatnode/public/main.js
+++ chatnode/public/main.js
@@ -18,13 +18,43 @@ let textInput = document.querySelector('#chatTextInput');
 loginPage.style.display = 'flex';
 chatPage.style.display = 'none';
 
+//função que renderiza usuários na tela
+function renderUserList(){
+    let ul = document.querySelector('.userList');
+    ul.innerHTML = '';
+
+    userList.forEach(i => {
+        ul.innerHTML += `<li>${i}</li>`;
+    })
+}
+
 //ação para receber o nome do loginNameInput e entrar na tela de chat
 loginInput.addEventListener('keyup', (e) => {
-    if(e.keyCode === 13) {
+    if(e.keyCode === 13) { //tecla 13 é o Enter
         let name = loginInput.value.trim();
-        if(name != ''){
+        if(name !== ''){
             username = name;
             document.title = `Chat (${username})`;
+
+            //emitter do join-request
+            socket.emit('join-request', username);
         }
     }
+});
+
+//listener do user-ok
+socket.on('user-ok', (connectedUsers) => {
+
+    //esconde tela de login, mostra tela do chat
+    loginPage.style.display = 'none';
+    chatPage.style.display = 'flex';
+
+    //dá foco no campo de texto do chat
+    textInput.focus();
+
+    //lista de usuários receber os usuários conectados
+    userList = connectedUsers;
+
+    //renderiza a lista de usuários na tela
+    renderUserList();
 });
--- chatnode/server.js
+++ chatnode/server.js
@@ -15,7 +15,25 @@ server.listen(3000);
 //ler a pasta public
 app.use(express.static(path.join(__dirname, 'public')));
 
+//variavel que irá armazenar lista de usuários que estarão conectados
+let connectedUsers = [];
+
 //listener do server (tbm tem que ter no client)
 io.on('connection', (socket) => {
     console.log("Conexão detectada...");
+
+    //listener do join-request, requisição de entrada (.on é sempre listener)
+    socket.on('join-request', (username) => {
+
+        //reconhecer a aba sendo usada pelo username inserido
+        socket.username = username;
+
+        //adicionar username na lista 
+        connectedUsers.push(username);
+        console.log(connectedUsers);
+
+        //emitter user-ok
+        socket.emit('user-ok', connectedUsers);
+    });
+
 })
